c8c8fec1f65dab1be3a117cbc12680ee9b6da5a4
gustavo <gustavonatan.2001.pereira@gmail.com>
2026-03-16 10:41:15 -0300

    integrado ao frontend

--- barbearia/barbearia/src/main/java/barbearia/barbearia/repository/AgendamentoRepository.java
+++ barbearia/barbearia/src/main/java/barbearia/barbearia/repository/AgendamentoRepository.java
@@ -1,7 +1,17 @@
 package barbearia.barbearia.repository;
 
 import barbearia.barbearia.entity.Agendamento;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+import java.time.LocalDateTime;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento,Long> {
+
+    @Transactional
+    @Modifying
+    void deleteById(Long id);
+
+    boolean existsByDataAgendada(LocalDateTime dataAgendada);
 }
--- barbearia/barbearia/src/main/java/barbearia/barbearia/service/AgendamentoService.java
+++ barbearia/barbearia/src/main/java/barbearia/barbearia/service/AgendamentoService.java
@@ -5,6 +5,8 @@ import barbearia.barbearia.repository.AgendamentoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -19,12 +21,24 @@ public class AgendamentoService {
 
 
     public Agendamento criaragendamento(@RequestBody Agendamento agendamento){
-        Agendamento ag = agendamentoRepository.save(agendamento);
-        return ag;
+        if(agendamentoRepository.existsByDataAgendada(agendamento.getDataAgendada())){
+            throw new RuntimeException("Horário já reservado");
+        }
+        return agendamentoRepository.save(agendamento);
     }
 
     @Transactional
     public List<Agendamento> listar(){
         return agendamentoRepository.findAll();
+    }
+
+
+    @Transactional
+    public  Agendamento deletarAgendamento(Long id){
+         Agendamento agDelete = agendamentoRepository.findById(id)
+                 .orElseThrow(() ->  new RuntimeException("Agendamento nao encontrado por id" + id));
+        agendamentoRepository.delete(agDelete);
+
+         return agDelete;
     }
 }
